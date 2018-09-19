89dcb8fe4dccac975657275c77cf39acff5c2a8d
somya <somya.1723@gmail.com>
2018-09-20 00:06:33 +0530

    added content

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,21 @@ import "../styles/style.css"
 import Layout from '../components/layout'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hello World</h1>
-    <p>I am Somya.</p>
-    <p>I create awesome web sites and web applications.</p>
+  <div className = "main">
+    <nav className = "nav-bar">
+      <ul className = "nav-link">
+        <li><a href="#">Home</a></li>
+        <li><a href = "#">About</a></li>
+        <li><a href = "#">Projects</a></li>
+        <li><a href = "#">Resume</a></li>
+        <li><a href = "#">Contact</a></li>
+      </ul>
+    </nav>
+    <div className = "intro">
+      <h1>Hello World</h1>
+      <p>I am Somya.</p>
+      <p>I create awesome web sites and web applications.</p>
+    </div>
   </div>
 )
 
