74fb0792d40d5e6e67114c18b36cf2b934ee2f67
somya <somya.1723@gmail.com>
2018-09-21 17:33:47 +0530

    about style css added

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import "../styles/about-style.css"
 
 const About = () => (
-  <main className = "context">
-    <h1>About Me</h1>
-    <p>I am Somya and I am web developer. The main accepts of my work are HTML, CSS and JavaScript.</p>
-    <p>I am a self taught web developer and surely it took time but was totaly worth it.</p>
-    <p></p>
+  <main className="context">
+
+    <div className="main-content">
+      <h1 className="heading">About Me</h1>
+      <p className = "para">
+        I am Somya and I am web developer. The main accepts of my work are HTML,
+        CSS and JavaScript.
+      </p>
+      <p className = "para">
+        I am a self taught web developer and surely it took time but was totaly
+        worth it.
+      </p>
+      <p className = "para">
+        I love learning and working with new things. Self teaching has been hard
+        but I would say that it is not impossible.
+      </p>
+      <p className = "para">
+        I am on the way of learning React.js and will accopmplish that in
+        meanwhile.
+      </p>
+    </div>
+
+    <div className = "resume">
+    <h1>/* Resume */</h1>
+    </div>
   </main>
 )
 
