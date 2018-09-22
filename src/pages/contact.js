ba137545373d2434ad4ae80617f32c0a83341467
somya <somya.1723@gmail.com>
2018-09-22 16:45:42 +0530

    some changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,30 +5,26 @@ import '..//styles/contact-style.css'
 
 const Contact = () => (
   <main className="contacts">
-
     <div className="contact-context">
       <p>
         You can contact me via <a href="mailto:somya.1723@gmailcom">email</a>
       </p>
       <p>You can also connect through:</p>
       <div className="contact-via">
-        <div class="wrapper-3">
-          <div className="contact-inside">
-            <a href="www.facebook.com">Facebook</a>
-          </div>
-          <div className="contact-inside">
-            <a href="www.twitter.com">Twitter</a>
-          </div>
-          <div className="contact-inside">
-            <a href="www.codepen.com">Codepen</a>
-          </div>
-          <div className="contact-inside">
-            <a href="www.github.com">Github</a>
-          </div>
+        <div className="contact-inside">
+          <a href="www.facebook.com">Facebook</a>
+        </div>
+        <div className="contact-inside">
+          <a href="www.twitter.com">Twitter</a>
+        </div>
+        <div className="contact-inside">
+          <a href="www.codepen.com">Codepen</a>
+        </div>
+        <div className="contact-inside">
+          <a href="www.github.com">Github</a>
         </div>
       </div>
     </div>
-    
   </main>
 )
 
