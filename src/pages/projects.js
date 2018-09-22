24c087d7af79fee71d04ab4a8e4aa57dcff72c55
somya <somya.1723@gmail.com>
2018-09-22 16:39:47 +0530

    added contact page

---
+++ src/pages/projects.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import "..//styles/project-style.css"
+
+const Projects = () => (
+  <main className = "projects">
+  <div className = "project-context">
+    <h1>Project Name</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint tempora rerum dolorum doloremque libero architecto? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae voluptatibus neque omnis veritatis totam!</p>
+    <h1>project 2</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil necessitatibus perferendis voluptatum provident consequuntur obcaecati eligendi praesentium quibusdam fugit sed? Suscipit aperiam ad in molestiae fuga odio ipsam nobis voluptas?</p>
+  </div>
+
+  </main>
+
+
+
+
+)
+
+export default Projects
